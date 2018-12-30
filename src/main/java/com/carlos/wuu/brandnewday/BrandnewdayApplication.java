8301bac6cad348f83387d80065c3ec27dbf89d6e
CarlosWuu <wujiabin@yudianbank.com>
2018-12-30 13:13:39 +0800

    init project

---
+++ src/main/java/com/carlos/wuu/brandnewday/BrandnewdayApplication.java
@@ -0,0 +1,14 @@
+package com.carlos.wuu.brandnewday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandnewdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandnewdayApplication.class, args);
+    }
+
+}
+
